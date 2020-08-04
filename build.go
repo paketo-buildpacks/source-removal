9f3f1ed13c99d705123adc38f2e90c2b4c99ae60
Forest Eckhardt <feckhardt@pivotal.io>
2020-08-04 17:59:10 -0400

    Minor refactor

--- build.go
+++ build.go
@@ -12,7 +12,7 @@ import (
 
 func Build() packit.BuildFunc {
 	return func(context packit.BuildContext) (packit.BuildResult, error) {
-		var globs []string
+		var globs = []string{context.WorkingDir}
 
 		for _, entry := range context.Plan.Entries {
 			existingGlobs, ok := entry.Metadata["keep"]
@@ -40,8 +40,8 @@ func Build() packit.BuildFunc {
 			// Output: ["working-dir/public", "working-dir/public/data", "working-dir/public/data/*"]
 			for _, glob := range rawGlobs {
 				dirs := strings.Split(glob, string(os.PathSeparator))
-				for i := 1; i <= len(dirs); i++ {
-					globs = append(globs, filepath.Join(context.WorkingDir, filepath.Join(dirs[:i]...)))
+				for i := range dirs {
+					globs = append(globs, filepath.Join(context.WorkingDir, filepath.Join(dirs[:i+1]...)))
 				}
 			}
 		}
@@ -49,11 +49,6 @@ func Build() packit.BuildFunc {
 		removalFunc := func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
-			}
-
-			// Prevents up from deleting the working directory ... Thanks filepath.Walk
-			if path == context.WorkingDir {
-				return nil
 			}
 
 			match, err := matchingGlob(path, globs)
@@ -92,7 +87,7 @@ func matchingGlob(path string, globs []string) (bool, error) {
 		}
 
 		if match {
-			// filepath.SkipDir is returned here becase this is a glob that
+			// filepath.SkipDir is returned here because this is a glob that
 			// specifies everything in a directroy. If we get a match on such
 			// a glob we want to ignore all other files in that directory because
 			// they are files we want to keep and the glob will not work
